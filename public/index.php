a6690470efcdd36d824f9430a804d6fd180d5558
Maryna <marynahaidashevska@gmail.com>
2019-05-10 23:04:14 -0500

    testing autoload 3

--- public/index.php
+++ public/index.php
@@ -9,11 +9,11 @@
 
 
 namespace classes;
-
+/*
 require __DIR__ . '/../lib/functions.php';
 require __DIR__ . '/../config/config.php';
 require __DIR__ . '/../classes/Validator.php';
-
+*/
 var_dump('here');die;
 try{
 /**
